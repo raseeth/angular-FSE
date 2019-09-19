d6244f03594cacc2455d94a5462f907cc9352fee
raseeth <raseeth.r@hotmail.com>
2019-09-19 15:00:49 +0100

    4 files modified, 2 added, 1 deleted

--- src/app/task-manager/task-router.module.ts
+++ src/app/task-manager/task-router.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { TASKROUTES } from "./task-routes";
+import { TaskAddComponent } from './task-add/task-add.component';
 import { TaskManagerComponent } from './task-manager.component';
 
 const taskRoutes: Routes = [
     { path: TASKROUTES.HOME, component: TaskManagerComponent,
         children: [
+          { path: TASKROUTES.ADD, component: TaskAddComponent },
         ],
     }
 ];
--- src/app/task-manager/task.module.ts
+++ src/app/task-manager/task.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from "@angular/core";
 import { TaskManagerComponent } from "./task-manager.component";
 import { TaskRouterModule } from "./task-router.module";
+import { TaskAddComponent } from './task-add/task-add.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @NgModule({
-    imports: [ TaskRouterModule ],
-    declarations: [ TaskManagerComponent ],
+    imports: [ TaskRouterModule, ReactiveFormsModule, FormsModule, CommonModule ],
+    declarations: [ TaskManagerComponent, TaskAddComponent ],
     providers: [  ]
 })
 
